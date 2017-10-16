248ba6a88ef89bce3dca075af3d55a5839a114b3
Oliver <oliverodd@gmail.com>
2017-10-16 20:35:58 +0200

    tested KeyboardInputs

---
+++ KeyboardInput/test_getch.py
@@ -0,0 +1,30 @@
+import curses
+
+def key_input(event):
+    if event == curses.KEY_LEFT:
+        print("Left Arrow Key pressed")
+    elif event == curses.KEY_RIGHT:
+        print("Right Arrow Key pressed")
+    elif event == curses.KEY_UP:
+        curses.endwin()
+    else:
+        print(event)
+
+screen = curses.initscr()
+try:
+    #curses.noecho()
+    #curses.curs_set(0)
+    screen.keypad(1)
+    screen.addstr("Press a key")
+except Exception:
+    print('Error')
+
+try:
+    while 1:
+        x = screen.getch()
+        key_input(x)
+except Exception:
+    print('Error')
+
+
+
